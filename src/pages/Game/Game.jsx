96631fba34a95dcee6e3bd7f74cc4fc6d9d052ae
kubat <kubat.aiupov@gmail.com>
2024-05-15 18:02:09 +0200

    add next btn, turn input to unctontrolled by useRef

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useRef, useContext } from "react";
+import React, { useState, useRef, useContext, useEffect, useMemo } from "react";
 import { DataContext } from "../../providers/DataProvider";
 import styles from "./Game.module.css";
 
 const Game = () => {
   const [gameStart, setGameStart] = useState(false);
+  const [currentCountry, setCurrentCountry] = useState(0)
   const [input, setInput] = useState("");
+  const inputRef = useRef();
 
   const { countriesData } = useContext(DataContext);
   console.log(countriesData);
@@ -15,24 +17,44 @@ const Game = () => {
   const getOneCountry = countriesData[idx];
   console.log(getOneCountry);
 
-  const handleInputChange = (e) => setInput(e.target.value);
+  // const getCountry = () => {
+  //   return countriesData[idx];
+  // }
+
+  //const handleInputChange = (e) => setInput(e.target.value);
+
+  //const handleInputRef = () => inputRef.current.value.toLowerCase();
 
   const submitInput = (e) => {
     e.preventDefault();
-    if (!input || input.trim().length === 0) {
+    if (!inputRef) {
       return;
     }
+    console.log(
+      getOneCountry.capital.slice(0)[0].toLowerCase() === inputRef.current.value.toLowerCase()
+    );
+    handleNext()
   };
+
+  const handleNext = () => {
+    let nextCountry = countriesData[randomIndex(countriesData)];
+    setCurrentCountry(nextCountry)
+  };
+
+  useEffect(() => {
+    // getCountry();
+  }, [submitInput]);
 
   return (
     <div>
       <div>
         <h4>{getOneCountry?.name?.common}</h4>
       </div>
-      {!input && <p>Please type the Capital name</p>}
+      {!inputRef && <p>Please type the Capital name</p>}
       <form onSubmit={submitInput}>
-        <input type="text" onChange={handleInputChange} value={input} />
+        <input type="text" ref={inputRef} />
         <button type="submit">Submit</button>
+        <button type="button" onClick={handleNext}>Next</button>
       </form>
     </div>
   );
