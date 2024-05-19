55b2199931a3647921f642e452962b6c6e0fd943
kubat <kubat.aiupov@gmail.com>
2024-05-19 12:55:23 +0200

    add start button, refactor next button

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -8,6 +8,7 @@ const Game = () => {
   const [input, setInput] = useState("");
   const [count, setCount] = useState(0);
   const [countCountries, setCountCountries] = useState(0);
+  const [showResult, setShowResult] = useState(false);
   const inputRef = useRef();
 
   const { countriesData } = useContext(DataContext);
@@ -17,7 +18,7 @@ const Game = () => {
   let idx = randomIndex(countriesData);
 
   const getOneCountry = countriesData[idx];
- // console.log(getOneCountry);
+  // console.log(getOneCountry);
 
   const handleCount = () => setCount(count + 1);
 
@@ -29,6 +30,15 @@ const Game = () => {
   //const handleInputChange = (e) => setInput(e.target.value);
 
   //const handleInputRef = () => inputRef.current.value.toLowerCase();
+
+  const handleShowResult = () => setShowResult(!showResult);
+
+  const handleStartGame = () => {
+    setGameStart(!gameStart);
+    handleShowResult();
+    setCount(0);
+    setCountCountries(0);
+  };
 
   const submitInput = (e) => {
     e.preventDefault();
@@ -48,13 +58,18 @@ const Game = () => {
       // );
     }
     inputRef.current.value = "";
-    handleNext();
     handleCountryCount();
   };
+  //handleNext();
 
   const handleNext = () => {
+    inputRef.current.value = getOneCountry?.capital?.slice(0)[0];
     let nextCountry = countriesData[randomIndex(countriesData)];
     setCurrentCountry(nextCountry);
+    handleCountryCount();
+    setTimeout(() => {
+      inputRef.current.value = "";
+    }, 1000);
   };
 
   // useEffect(() => {
@@ -63,21 +78,38 @@ const Game = () => {
 
   return (
     <div>
-      <div>
-        <h4>{getOneCountry?.name?.common}</h4>
-        <p>Total: {countCountries}</p>
-        <p>Correct: {count}</p>
-      </div>
-      {!inputRef && <p>Please type the Capital name</p>}
-      <form onSubmit={submitInput}>
-        <input type="text" ref={inputRef} />
-        <button type="submit">Submit</button>
-        <button type="button" onClick={handleNext}>
-          Next
-        </button>
-      </form>
+      <button type="button" onClick={handleStartGame}>
+        {gameStart ? "Stop" : "Start Game"}
+      </button>
+      {gameStart && (
+        <>
+          <div>
+            <h4>{getOneCountry?.name?.common}</h4>
+            <p>Total: {countCountries}</p>
+            <p>Correct: {count}</p>
+          </div>
+          {!inputRef && <p>Please type the Capital name</p>}
+          <form onSubmit={submitInput}>
+            <input type="text" ref={inputRef} />
+            <button type="submit">Submit</button>
+            <button type="button" onClick={handleNext}>
+              I do not know
+            </button>
+          </form>
+        </>
+      )}
+      {!gameStart && showResult && (
+        <div>
+          <h6>Total: {countCountries}</h6>
+          <h6>Correct: {count}</h6>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Game;
+
+//TODO
+// Conditionally render the total result after user clicks stop buttton
+// MODAL?? maybe
