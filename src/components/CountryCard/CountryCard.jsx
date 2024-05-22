bb8f2dd3ffe9c1dd12ad09638d3e31a0f35e49af
kubat <kubat.aiupov@gmail.com>
2024-05-22 12:35:17 +0200

    add style cards with bootstrap

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./CountryCard.module.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./CountryCard.css";
 
-const CountryCard = ({ name, city, flag,id }) => {
-
-  
- 
+const CountryCard = ({ name, city, flag, id }) => {
   return (
-    <div className={styles['card-container']}>
-      <header className={styles["card-header"]}>
-        <h2 className={styles["card-title"]} >{name}</h2>
-        <h4 className={styles["card-city"]}>{city}</h4>
-      </header>
-      <img src={flag} alt="image of country's flag" className={styles["card-flag"]} />
-      <Link to={`countries/${id}`} className={styles["card-btn"]}>Learn More...</Link>
-    </div>
+    <Card
+      style={{ width: "18rem", height: "25rem" }}
+      border="secondary"
+      variant="info"
+      className="card text-center"
+    >
+      <Card.Img
+        variant="top"
+        src={flag}
+        alt="image of country's flag"
+        style={{ height: "10rem", width: "auto" }}
+      />
+      <Card.Body className="ml-5 md mt-3">
+        <Card.Title className="fs-3 fw-bolder">{name}</Card.Title>
+        <Card.Subtitle className="fs-5 mt-1">{city}</Card.Subtitle>
+      </Card.Body>
+      <Link
+        to={`countries/${id}`}
+        variant="primary"
+        style={{ color: "inherit" }}
+      >
+        <Button variant="primary" className="mb-3">
+          Learn more
+        </Button>
+      </Link>
+    </Card>
   );
 };
 
