7af4f3adce33dd6f4b986b53a150f340c36673c6
kubat <kubat.aiupov@gmail.com>
2024-05-23 18:20:59 +0200

    add define card for country

--- src/pages/Country/Country.jsx
+++ src/pages/Country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { DataContext } from "../../providers/DataProvider";
-import { Container, Card, Button } from "react-bootstrap";
+import { Container, Card, Button, Row, Col, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./Country.module.css";
 
@@ -20,17 +20,69 @@ const Country = () => {
   console.log(findOneCountry);
 
   return (
-    <Container className="country-container" style={{ width: '80rem', display: 'flex', flexDirection: 'column' }}>
-      <Card className="shadow-sm bg-white rounded" >
-         <Card.Body>
-          <Card.Title>{id}</Card.Title>
-          <Card.Title>Area</Card.Title>
-          <Card.Text>{findOneCountry?.area}m2</Card.Text>
-        
-        {findOneCountry?.population}
-        <button onClick={navigateBack}>Back</button>
-          </Card.Body> 
-      </Card>
+    <Container fluid className="country-container mt-5">
+      <Row>
+        <Col>
+          <Card className="shadow-sm bg-white rounded">
+            <Card.Body className="d-flex flex-column justify-content-between align-items-center">
+              <Card.Title className="text-center fs-1 m-2">Details</Card.Title>
+              <Card.Title className="fs-2">
+                <span className="badge text-secondary fs-5">Country name:</span>{" "}
+                {id}
+              </Card.Title>
+              <Card.Title className="fs-2">
+                <span className="badge text-secondary fs-5">
+                  Capitail City:
+                </span>
+                {findOneCountry?.capital}
+              </Card.Title>
+              <Card.Title className="fs-2">
+                <span className="badge text-secondary fs-5">Area:</span>
+                {findOneCountry?.area}m2
+              </Card.Title>
+              <Card.Title className="fs-2">
+                <span className="badge text-secondary fs-5">Population:</span>
+                {findOneCountry?.population}
+              </Card.Title>
+              <Card.Title className="fs-2">
+                <span className="badge text-secondary fs-5">Region:</span>
+                {findOneCountry?.continents[0]}
+              </Card.Title>
+              <Card.Title className="fs-2">
+                <span className="badge text-secondary fs-5">Borders with:</span>
+                {findOneCountry?.borders.map((land) => (
+                  <li key={land}>{land}</li>
+                ))}
+              </Card.Title>
+              <Card.Title className="fs-2">
+                <span className="badge text-secondary fs-5">Languages:</span>
+                {findOneCountry &&
+                  Object.values(findOneCountry?.languages).map((lang) => (
+                    <li key={lang}>{lang}</li>
+                  ))}
+              </Card.Title>
+              <Image
+                rounded
+                src={findOneCountry?.flags.svg}
+                alt="image of country's flag"
+                style={{ height: "10rem", width: "auto" }}
+                className="m-3"
+              />
+
+              <Button variant="primary" onClick={navigateBack}>
+                Back
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title className="text-center">Map</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
