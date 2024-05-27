6637956875a135fd3781bb83d64f4c4385e508d5
kubat <kubat.aiupov@gmail.com>
2024-05-27 12:54:36 +0200

    fix marker, init new icon

--- src/pages/Country/Country.jsx
+++ src/pages/Country/Country.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { DataContext } from "../../providers/DataProvider";
 import {
@@ -12,14 +12,16 @@ import {
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
+import locationIcon from '../../assets/location-pin.png'
 import styles from "./Country.module.css";
 
 const Country = () => {
   const { countriesData } = useContext(DataContext);
   const { id } = useParams();
   const navigate = useNavigate();
-  const mapRef = useRef();
+
 
   const navigateBack = () => navigate(-1);
 
@@ -35,6 +37,12 @@ const Country = () => {
     return null;
   }
 
+  // icon for marker location
+  const countryIcon = new Icon({
+    iconUrl: locationIcon,
+    iconSize: [38, 38]
+  })
+
   console.log(findOneCountry);
 
   return (
@@ -44,41 +52,7 @@ const Country = () => {
           <Card className="shadow-sm bg-white rounded">
             <Card.Body className="d-flex flex-column justify-content-between align-items-center">
               <Card.Title className="text-center fs-1 m-2">Details</Card.Title>
-              {/*<Card.Title className="fs-2">
-                <span className="badge text-secondary fs-5">Country name:</span>{" "}
-                {id}
-              </Card.Title>
-              <Card.Title className="fs-2">
-                <span className="badge text-secondary fs-5">
-                  Capitail City:
-                </span>
-                {findOneCountry?.capital}
-              </Card.Title>
-              <Card.Title className="fs-2">
-                <span className="badge text-secondary fs-5">Area:</span>
-                {findOneCountry?.area?.toLocaleString()}m2
-              </Card.Title>
-              <Card.Title className="fs-2">
-                <span className="badge text-secondary fs-5">Population:</span>
-                {findOneCountry?.population.toLocaleString()}
-              </Card.Title>
-              <Card.Title className="fs-2">
-                <span className="badge text-secondary fs-5">Region:</span>
-                {findOneCountry?.continents[0]}
-              </Card.Title>
-              <Card.Title className="fs-2">
-                <span className="badge text-secondary fs-5">Borders with:</span>
-                {findOneCountry?.borders?.map((land) => (
-                  <li key={land}>{land}</li>
-                ))}
-              </Card.Title>
-              <Card.Title className="fs-2">
-                <span className="badge text-secondary fs-5">Languages:</span>
-                {findOneCountry &&
-                  Object.values(findOneCountry?.languages).map((lang) => (
-                    <li key={lang}>{lang}</li>
-                  ))}
-                  </Card.Title>*/}
+    
               <Table responsive='lg' striped bordered hover>
                 <thead>
                   <tr>
@@ -109,14 +83,14 @@ const Country = () => {
                   </tr>
                   <tr>
                     <td>Borders </td>
-                    <td className="fw-bold">{findOneCountry?.borders.toString()}</td>
+                    <td className="fw-bold">{findOneCountry?.borders?.toString()}</td>
                   </tr>
                   <tr>
                     <td>Language</td>
                     <td className="fw-bold">{findOneCountry &&
-                  Object.values(findOneCountry?.languages).map((lang) => (
-                    <div key={lang}>{lang}</div>
-                  ))}</td>
+                      Object.values(findOneCountry?.languages).map((lang) => (
+                        <div key={lang}>{lang}</div>
+                      ))}</td>
                   </tr>
                 </tbody>
               </Table>
@@ -141,17 +115,16 @@ const Country = () => {
               <MapContainer
                 center={[lat, long]}
                 zoom={7}
-                ref={mapRef}
                 style={{ height: "700px", width: "100%" }}
               >
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 >
-                  <Marker position={[lat, long]}>
+                </TileLayer>
+                  <Marker position={[lat, long]} icon={countryIcon}>
                     <Popup>{findOneCountry?.capital} - Capital City</Popup>
                   </Marker>
-                </TileLayer>
               </MapContainer>
             </Card.Body>
           </Card>
