d713c891c11e4ec8e883b6441892f89bb5fe9b84
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2024-06-02 12:49:14 +0300

    add hamburger menu

--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -7,6 +7,7 @@ import { Button } from "react-bootstrap";
 
 const NavbarComponent = () => {
   const [showButton, setShowButton] = useState(false);
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   const handleScroll = () => {
     const scrollPosition = window.scrollY;
@@ -27,42 +28,55 @@ const NavbarComponent = () => {
     };
   }, []);
 
+  const handleToggle = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
+
+  const handleNavClick = () => {
+    setIsNavExpanded(false);
+  };
+
   return (
     <Navbar
       bg="dark"
-      data-bs-theme="dark"
       expand="md"
       fixed="top"
       className="bg-body-tertiary"
+      data-bs-theme="dark"
+      expanded={isNavExpanded}
     >
       <Container>
         <Navbar.Brand href="/">Countries App</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/game">
-            Play Game
-          </Nav.Link>
-          <Nav.Link as={Link} to="/population">
-            Population
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="https://github.com/kraft-aka/react-countries-app"
-            target="_blank"
-          >
-            Code
-          </Nav.Link>
-        </Nav>
-        {showButton && (
-          <Button
-            variant="outline-success"
-            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-          >
-            Back to top
-          </Button>
-        )}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/" onClick={handleNavClick}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/game" onClick={handleNavClick}>
+              Play Game
+            </Nav.Link>
+            <Nav.Link as={Link} to="/population" onClick={handleNavClick}>
+              Population
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="https://github.com/kraft-aka/react-countries-app"
+              target="_blank"
+              onClick={handleNavClick}
+            >
+              Code
+            </Nav.Link>
+          </Nav>
+          {showButton && (
+            <Button
+              variant="outline-success"
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+              Back to top
+            </Button>
+          )}
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
