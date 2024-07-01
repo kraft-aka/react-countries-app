fcca0d6a9daf4fdd09fbe0197f4d86432c092d32
kubat <kubat.aiupov@gmail.com>
2024-07-01 19:16:38 +0200

    add continentFact comp

---
+++ src/components/Facts/ContinentFact/ContinentFact.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const ContinentFact = ({
+  countriesInAsia,
+  countriesInEurope,
+  countriesInNorthAmerica,
+  countriesInSouthAmerica,
+  countriesInOceania,
+  countriesInAfrica,
+  countriesInAntarctica,
+}) => {
+  const dataSet = {
+    labels: [
+      "Asia",
+      "Europe",
+      "North America",
+      "South America",
+      "Africa",
+      "Oceania",
+      "Antarctica",
+    ],
+    datasets: [
+      {
+        label: "Number of countries",
+        data: [
+          countriesInAsia.length,
+          countriesInEurope.length,
+          countriesInNorthAmerica.length,
+          countriesInSouthAmerica.length,
+          countriesInAfrica.length,
+          countriesInOceania.length,
+          countriesInAntarctica.length,
+        ],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255,215,0, 0.2)",
+          "rgba	(152,251,152, 0.2)",
+          "rgba (186,85,211, 0.2)",
+          "rgba (250,235,215, 0.2)",
+          "rgba (255,250,205, 0.2)",
+        ],
+        borderColor: ["rgba(255, 99, 132, 1)"],
+        borderWidth: 2,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h2>Number of Continents per Continent</h2>
+
+      <section style={{ width: "350px" }}>
+        {<Pie data={dataSet} options={{ responsive: true }} />}
+      </section>
+    </div>
+  );
+};
+
+export default ContinentFact;
--- src/pages/FactsData/FactsData.jsx
+++ src/pages/FactsData/FactsData.jsx
@@ -1,19 +1,54 @@
 import React, { useContext } from "react";
 import { DataContext } from "../../providers/DataProvider";
 import UNFact from "../../components/Facts/UNFact/UNFact";
+import ContinentFact from "../../components/Facts/ContinentFact/ContinentFact";
 
 const FactsData = () => {
-
   const { countriesData } = useContext(DataContext);
 
   // data of UN Member countries and those which are not members
-  const isUnMember = countriesData.filter(country=> country.unMember);
-  const isNotUnMember = countriesData.filter(country=> country.unMember !== true);
+  const isUnMember = countriesData.filter((country) => country.unMember);
+  const isNotUnMember = countriesData.filter(
+    (country) => country.unMember !== true
+  );
 
-  //console.log(countriesData, isUnMember, isNotUnMember);
+  // data for continent
+  const countriesInAsia = countriesData.filter(
+    (country) => country.continents == "Asia"
+  );
+  const countriesInEurope = countriesData.filter(
+    (country) => country.continents == "Europe"
+  );
+  const countriesInNorthAmerica = countriesData.filter(
+    (country) => country.continents == "North America"
+  );
+  const countriesInSouthAmerica = countriesData.filter(
+    (country) => country.continents == "South America"
+  );
+  const countriesInAfrica = countriesData.filter(
+    (country) => country.continents == "Africa"
+  );
+  const countriesInOceania = countriesData.filter(
+    (country) => country.continents == "Oceania"
+  );
+  const countriesInAntarctica = countriesData.filter(
+    (country) => country.continents == "Antarctica"
+  );
+
+  console.log(countriesData);
   return (
     <div>
-      <UNFact isUnMember={isUnMember} isNotUnMember={ isNotUnMember }/>
+      <h1>Some numeric data about countries</h1>
+      <UNFact isUnMember={isUnMember} isNotUnMember={isNotUnMember} />
+      <ContinentFact
+        countriesInAsia={countriesInAsia}
+        countriesInEurope={countriesInEurope}
+        countriesInNorthAmerica={countriesInNorthAmerica}
+        countriesInSouthAmerica={countriesInSouthAmerica}
+        countriesInOceania={countriesInOceania}
+        countriesInAfrica={countriesInAfrica}
+        countriesInAntarctica={countriesInAntarctica}
+      />
     </div>
   );
 };
