840f5dddc552b5a0d5bc9ca3f3768f6820b73d35
kubat <kubat.aiupov@gmail.com>
2024-07-02 14:05:21 +0200

    add style, grid and go back btn to factsData

--- src/pages/FactsData/FactsData.jsx
+++ src/pages/FactsData/FactsData.jsx
@@ -1,11 +1,18 @@
 import React, { useContext } from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { DataContext } from "../../providers/DataProvider";
+import { useNavigate } from "react-router-dom";
 import UNFact from "../../components/Facts/UNFact/UNFact";
 import ContinentFact from "../../components/Facts/ContinentFact/ContinentFact";
 import AccessToOceanFact from "../../components/Facts/AccessToOceanFact/AccessToOceanFact";
 
 const FactsData = () => {
   const { countriesData } = useContext(DataContext);
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate("..", { relative: "path" });
+  };
 
   // data of UN Member countries and those which are not members
   const isUnMember = countriesData.filter((country) => country.unMember);
@@ -43,29 +50,51 @@ const FactsData = () => {
   );
   const isLandlocked = countriesData.filter((country) => country.landlocked);
 
-
   // array of languages
   const langs = countriesData.flatMap(({ languages }) => languages);
 
   console.log(countriesData);
   return (
-    <div>
-      <h1>Some numeric data about countries</h1>
-      <UNFact isUnMember={isUnMember} isNotUnMember={isNotUnMember} />
-      <ContinentFact
-        countriesInAsia={countriesInAsia}
-        countriesInEurope={countriesInEurope}
-        countriesInNorthAmerica={countriesInNorthAmerica}
-        countriesInSouthAmerica={countriesInSouthAmerica}
-        countriesInOceania={countriesInOceania}
-        countriesInAfrica={countriesInAfrica}
-        countriesInAntarctica={countriesInAntarctica}
-      />
-      <AccessToOceanFact
-        isNotLandlocked={isNotLandlocked}
-        isLandlocked={isLandlocked}
-      />
-    </div>
+    <Container fluid>
+      <Card className="mt-4 p-2 pb-5 ">
+        <Card.Header className="p-1 m-0">
+          <Card.Title className="m-4 text-center fs-1">
+            Some numeric data about countries
+          </Card.Title>
+        </Card.Header>
+        <Row>
+          <Col lg={4}>
+            <UNFact isUnMember={isUnMember} isNotUnMember={isNotUnMember} />
+          </Col>
+          <Col lg={4}>
+            <ContinentFact
+              countriesInAsia={countriesInAsia}
+              countriesInEurope={countriesInEurope}
+              countriesInNorthAmerica={countriesInNorthAmerica}
+              countriesInSouthAmerica={countriesInSouthAmerica}
+              countriesInOceania={countriesInOceania}
+              countriesInAfrica={countriesInAfrica}
+              countriesInAntarctica={countriesInAntarctica}
+            />
+          </Col>
+          <Col lg={4}>
+            <AccessToOceanFact
+              isNotLandlocked={isNotLandlocked}
+              isLandlocked={isLandlocked}
+            />
+          </Col>
+        </Row>
+
+        <Button
+          onClick={goBack}
+          className="m-4"
+          variant="success"
+          style={{ width: "100px", alignText: "center" }}
+        >
+          Go Back
+        </Button>
+      </Card>
+    </Container>
   );
 };
 
