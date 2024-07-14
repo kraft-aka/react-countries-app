dcb1a39b310d05f42ba5de001dcb79fa7b5c9427
kubat <kubat.aiupov@gmail.com>
2024-07-14 17:37:40 +0200

    add btns to accessToOceanFact comp

--- src/components/Facts/AccessToOceanFact/AccessToOceanFact.jsx
+++ src/components/Facts/AccessToOceanFact/AccessToOceanFact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Card, Button, ListGroup } from "react-bootstrap";
@@ -6,6 +6,10 @@ import { Card, Button, ListGroup } from "react-bootstrap";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const AccessToOceanFact = ({ isNotLandlocked, isLandlocked }) => {
+
+  const [showLandlocked, setShowLandlocked] = useState(false)
+  const [showCoastal, setShowCoastal] = useState(false)
+
   const dataSet = {
     labels: ["not Landlocked", "Landlocked"],
     datasets: [
@@ -33,7 +37,54 @@ const AccessToOceanFact = ({ isNotLandlocked, isLandlocked }) => {
           {<Pie data={dataSet} options={{ responsive: true }} />}
         </section>
       </Card.Body>
-      <Card.Footer>
+      <ListGroup variant="flush">
+        <ListGroup.Item>
+          <Button
+            onClick={() => setShowCoastal((prev) => !prev)}
+            style={{
+              backgroundColor: "rgba(255, 99, 132, 1)",
+              border: "1px solid rgba(255, 99, 132, 1)",
+            }}
+          >
+            {showCoastal ? "Hide" : "Show Coastal Countries"}
+          </Button>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Button
+            onClick={() => setShowLandlocked((prev) => !prev)}
+            style={{
+              backgroundColor: "rgba(54, 162, 235, 1)",
+              border: "1px solid rgba(54, 162, 235, 1)",
+            }}
+          >
+            {showLandlocked ? "Hide" : "Show Landlocked Countries"}
+          </Button>
+        </ListGroup.Item>
+      </ListGroup>
+      <Card.Footer className="d-flex align-items-center justify-content-start flex-wrap">
+        {isNotLandlocked &&
+          showCoastal &&
+          isNotLandlocked.map((i) => (
+            <Button
+              className="m-2 p-1"
+              variant="outline-success"
+              key={i?.name?.common}
+            >
+              {i?.name?.common}
+            </Button>
+          ))}
+        <br />
+        {isLandlocked &&
+          showLandlocked &&
+          isLandlocked.map((i) => (
+            <Button
+              className="m-2 p-1"
+              variant="outline-success"
+              key={i?.name?.common}
+            >
+              {i?.name?.common}
+            </Button>
+          ))}
 
       </Card.Footer>
     </Card>
